7de6ecae1d14096388e10fbfcaa4a4415aeb0c53
Andrefty <andrei.farcasanu2002@gmail.com>
2018-12-28 15:12:53 +0200

    Added mapbox

--- app/src/main/java/guideme/kappa/ro/guideme/MapActivity.java
+++ app/src/main/java/guideme/kappa/ro/guideme/MapActivity.java
@@ -1,31 +1,68 @@
 package guideme.kappa.ro.guideme;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MapActivity extends AppCompatActivity {
+    private MapView mapView;
 
-    private Button b;
+
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiYW5kcmVmdHkiLCJhIjoiY2pxODE4MGcxMm5ldzQ4ank2dXg5b29rZSJ9.KxoOIJBxaY0ce5Il6apEWQ");
         setContentView(R.layout.activity_map);
-        b = findViewById(R.id.but69);
-        firebaseAuth = FirebaseAuth.getInstance();
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebaseAuth.signOut();
-                startActivity(new Intent(MapActivity.this, FirstActivity.class));
-            }
-        });
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 }
