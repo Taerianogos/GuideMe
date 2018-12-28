ce67c88dbf8d995df2d86780f58d59c6d81fb8d5
Stefan <cucust9876@yahoo.com>
2018-12-28 19:32:52 +0200

    Sign out button

--- app/src/main/java/guideme/kappa/ro/guideme/MapActivity.java
+++ app/src/main/java/guideme/kappa/ro/guideme/MapActivity.java
@@ -1,7 +1,11 @@
 package guideme.kappa.ro.guideme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 
@@ -10,8 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MapActivity extends AppCompatActivity {
     private MapView mapView;
-
-
+    private Button b;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -20,6 +23,15 @@ public class MapActivity extends AppCompatActivity {
         Mapbox.getInstance(this, "pk.eyJ1IjoiYW5kcmVmdHkiLCJhIjoiY2pxODE4MGcxMm5ldzQ4ank2dXg5b29rZSJ9.KxoOIJBxaY0ce5Il6apEWQ");
         setContentView(R.layout.activity_map);
         mapView = (MapView) findViewById(R.id.mapView);
+        b = findViewById(R.id.buttonTest);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MapActivity.this, ThirdActivity.class));
+                finish();
+            }
+        });
    //     mapView.onCreate(savedInstanceState);
 
     }
